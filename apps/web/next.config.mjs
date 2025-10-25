4524f740bebe7a245831779c7d5b3f6a750c7e4d
ceeriil <simonorugbo15@gmail.com>
2025-10-25 08:21:14 +0100

    deal details

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'reviews.web3', 'images.web3deals.io'],
+    domains: [
+      'images.pexels.com',
+      'reviews.web3',
+      'images.web3deals.io',
+      'cdn.web3deals.io',
+      'images.web3',
+    ],
   },
 };
 
