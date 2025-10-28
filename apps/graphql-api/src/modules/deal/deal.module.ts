399e414724801b273757e02bc7f9fac4f7ee53b8
Angry Coding <mailidanielifechukwu@gmail.com>
2025-10-28 16:06:15 +0000

    chore: models

--- apps/graphql-api/src/modules/deal/deal.module.ts
+++ apps/graphql-api/src/modules/deal/deal.module.ts
@@ -1,10 +1,10 @@
-import { DealEntity } from '@monkedeals/postgresql-typeorm';
+import { DealEntity, DealOptionEntity } from '@monkedeals/postgresql-typeorm';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DealResolver } from './deal.resolver';
 import { DealService } from './deal.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([DealEntity])],
+  imports: [TypeOrmModule.forFeature([DealEntity, DealOptionEntity])],
   providers: [DealResolver, DealService],
 })
 export class DealModule {}
--- apps/graphql-api/src/modules/deal/deal.service.ts
+++ apps/graphql-api/src/modules/deal/deal.service.ts
@@ -1,11 +1,17 @@
 import { ErrorCode } from '@/constants/error-code.constant';
 // import { ValidationException } from '@monkedeals/graphql';
-import { DealEntity } from '@monkedeals/postgresql-typeorm';
+import {
+  DealEntity,
+  DealLocationEntity,
+  DealOptionEntity,
+} from '@monkedeals/postgresql-typeorm';
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 // import { UpdateUserInput } from './dto/deal.dto';
 import { ValidationException } from '@monkedeals/graphql/exceptions/validation.exception';
+import { DealLocation } from './model/deal-location.model';
+import { DealOption } from './model/deal-option.model';
 import { Deal } from './model/deal.model';
 @Injectable()
 export class DealService {
@@ -14,6 +20,12 @@ export class DealService {
   constructor(
     @InjectRepository(DealEntity)
     private readonly dealRepository: Repository<DealEntity>,
+
+    @InjectRepository(DealOptionEntity)
+    private readonly dealOptionRepository: Repository<DealOptionEntity>,
+
+    @InjectRepository(DealLocationEntity)
+    private readonly dealLocationRepository: Repository<DealLocationEntity>,
   ) {}
 
   async get(id: number): Promise<Deal> {
@@ -32,5 +44,33 @@ export class DealService {
     });
 
     return deals;
+  }
+
+  async getDealOption(id: number): Promise<DealOption[]> {
+    await this.ensureDealExists(id);
+    const dealOptions = await this.dealOptionRepository
+      .createQueryBuilder('option')
+      .where('option.deal_id = :dealId', { dealId: id })
+      .getMany();
+
+    return dealOptions;
+  }
+
+  async getDealLocation(id: number): Promise<DealLocation> {
+    await this.ensureDealExists(id);
+    const location = await this.dealLocationRepository
+      .createQueryBuilder('location')
+      .where('location.deal_id = :dealId', { dealId: id })
+      .getOne();
+    return { ...location };
+  }
+
+  private async ensureDealExists(id: number): Promise<void> {
+    const deal = await this.dealRepository.findOneByOrFail({
+      id,
+    });
+    if (!deal) {
+      throw new ValidationException(ErrorCode.E003, 'Deal not found');
+    }
   }
 }
